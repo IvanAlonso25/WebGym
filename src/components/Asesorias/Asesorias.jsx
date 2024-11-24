20e0abe2ebc8245216e0753e0ef536e90e5a8430
IvanAlonso25 <ialonso479@gmail.com>
2024-11-24 19:48:30 +0100

    Asesorias modificado

--- src/components/Asesorias/Asesorias.jsx
+++ src/components/Asesorias/Asesorias.jsx
@@ -1,19 +1,23 @@
 import { Link } from '../Link';
+import '../../styles/Asesorias.css';
 
 export function Asesorias() {
     return (
         <>
-            <div className="card">
+            <div className='title'>
                 <h1>ASESORIAS</h1>
+            </div>
+            <div>
                 <div>
-                    <p>Plan totalmente personalizado exclusivamente para cada persona.</p>
+                    <h3>
+                        ¿Quieres mejorar tu rendimiento deportivo? ¿Te gustaría tener un seguimiento personalizado y adaptado a tus necesidades? 
+                        ¿Quieres mejorar tu alimentación y tus entrenamientos? 
+                        Tenemos un plan perfecto para ti, con un seguimiento personalizado y adaptado a tus necesidades, nutrición personalizada y entrenamientos adaptados a ti.
+                    </h3>
                 </div>
-                <div>
-                    <h1>Entrenamiento + Nutrición + Seguimiento</h1>
-                </div>
-                <div className="btnComprar">
-                    <Link to="/Comprar">Comprar <strong>(300€ / mes)</strong></Link>
-                </div>
+            </div>
+            <div className="btnComprarEspecificoNutricion">
+                <Link to="/Comprar">Comprar <strong>(300€ / mes)</strong></Link>
             </div>
         </>
     )
